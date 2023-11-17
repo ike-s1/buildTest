605486bc8a0f1dc323256b46ad04729dce819297
ike-s1 <vitalik.fedorchak1111@gmail.com>
2023-11-17 13:49:04 +0200

    ttt

--- src/components/Sources/Files/Files.js
+++ src/components/Sources/Files/Files.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Files.scss";
+import { FileUploader } from "react-drag-drop-files";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteFile, setFiles } from "../../../redux/slices/chatbot/slice";
 import { UploadedList } from "../../Shared/UploadedList/UploadedList";
@@ -53,7 +54,26 @@ const handleDeleteFile = (name) => {
       <div className="upload-files-container">
         <div className="upload-file-content">
           <div className="file-uplaoder">
-            
+            <FileUploader
+              multiple={true}
+              handleChange={handleUploadChange}
+              name="file"
+              types={fileTypes}
+            >
+              <div className={`uploader-content`}>
+                <img className={"upload-icon"} src={uploadIcon} alt="icon"/>
+                <p className={`upload-info`}>
+                  <span className={`upload-text`}>
+                    Drag &amp; Drop files here, or click to select files
+                    <br />
+                  </span>
+                  <span className={`upload-supported`}>
+                    {" "}
+                    Supported file types: .pdf, .doc, .docx, .txt
+                  </span>
+                </p>
+              </div>
+            </FileUploader>
           </div>
         </div>
         <UploadedList
